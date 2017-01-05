6c7b32d334d62b77de209b134b62e3c433af5786
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-05 08:20:39 -0500

    Cleaned up  'off'

--- src/main/Component/Component.prototype.off.js
+++ src/main/Component/Component.prototype.off.js
@@ -5,26 +5,29 @@ Component.prototype.off = function (name, callback) {
     this.subscribers = {};
   }
 
-  name.split(',').forEach(function (a) {
-    var i;
+  name
+    .toLowerCase()
+    .split(',')
+    .forEach(function (a) {
+      var i;
 
-    a = a.trim();
+      a = a.trim();
 
-    if (typeof self.subscribers[a] !== 'undefined') {
-      i = self.subscribers[a].indexOf(callback);
-
-      while (i !== -1) {
-        self.subscribers[a].splice(i, 1);
+      if (typeof self.subscribers[a] !== 'undefined') {
         i = self.subscribers[a].indexOf(callback);
-      }
 
-      if (typeof callback === 'undefined') {
-        while (self.subscribers[a].length) {
-          self.subscribers[a].shift();
+        while (i !== -1) {
+          self.subscribers[a].splice(i, 1);
+          i = self.subscribers[a].indexOf(callback);
+        }
+
+        if (typeof callback === 'undefined') {
+          while (self.subscribers[a].length) {
+            self.subscribers[a].shift();
+          }
         }
       }
-    }
-  });
+    });
 
   return this;
 };
