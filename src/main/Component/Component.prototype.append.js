3aa99532646a1b72c1c60b15ace948fa3e3f12a8
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-05 08:22:16 -0500

    sets parentNode and childNodes

--- src/main/Component/Component.prototype.append.js
+++ src/main/Component/Component.prototype.append.js
@@ -1,4 +1,15 @@
-Component.prototype.append = function (a) {
-  var target = Component.prototype.target.call(this, this.node.document);
-  target.append(a);
+Component.prototype.append = function (children) {
+  var self = this;
+
+  this.childNodes = this.childNodes || [];
+
+  this.mapChildrenToNode(children);
+  this.node.document.append(children);
+  [].push.apply(this.childNodes, children);
+
+  children.forEach(function (child) {
+    child.parentNode = self;
+  });
+
+  return this;
 };
