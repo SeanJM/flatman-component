7916510e93541b0b4133b979ace99da845f71f8d
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-05 08:23:07 -0500

    removed 'triggerMount'

--- src/main/Component/Component.prototype.mount.js
+++ src/main/Component/Component.prototype.mount.js
@@ -5,10 +5,6 @@ Component.prototype.mount = function () {
     if (element.node.document) {
       element.node.document.trigger('mount');
     }
-
-    if (element.elements) {
-      element.elements.forEach(triggerMount);
-    }
   }
 
   triggerMount(this);
