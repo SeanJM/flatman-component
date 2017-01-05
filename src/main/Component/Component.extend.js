daf9410942bd4f104011ac9548015e6f913a589e
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-05 08:22:58 -0500

    'facade.append'

--- src/main/Component/Component.extend.js
+++ src/main/Component/Component.extend.js
@@ -3,6 +3,9 @@ Component.extend = function () {
   var n = arguments.length;
 
   function each(a) {
+    if (typeof a.prototype.append === 'function') {
+      a.prototype.append = facade.append(a.prototype.append);
+    }
     for (var k in Component.prototype) {
       if (typeof a.prototype[k] === 'undefined') {
         a.prototype[k] = Component.prototype[k];
