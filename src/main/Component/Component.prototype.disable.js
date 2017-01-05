20837ad6ee549b0a44aa8213fc2ce1565fbed6cb
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-05 08:21:22 -0500

    Renamed 'dict.elements' to 'childNodes'

--- src/main/Component/Component.prototype.disable.js
+++ src/main/Component/Component.prototype.disable.js
@@ -1,13 +1,12 @@
 Component.prototype.disable = function () {
   var i = 0;
-  var elements = this.dict.elements;
-  var n = this.dict.elements.length;
+  var n = this.childNodes.length;
 
-  this.dict.disabledElements = elements.filter(a => a.dict.isDisabled);
+  this.dict.disabledElements = this.childNodes.filter(function (a) { return a.dict.isDisabled; });
   this.dict.isDisabled = true;
 
   for (; i < n; i++) {
-    elements[i].disable();
+    this.childNodes[i].disable();
   }
 
   this.node.document.disable();
--- src/main/Component/Component.prototype.enable.js
+++ src/main/Component/Component.prototype.enable.js
@@ -1,14 +1,13 @@
 Component.prototype.enable = function () {
   var i = 0;
-  var n = this.dict.elements.length;
-  var elements = this.dict.elements;
+  var n = this.childNodes.length;
   var disabledElements = this.dict.disabledElements;
 
   this.dict.isDisabled = false;
 
   for (; i < n; i++) {
-    if (disabledElements.indexOf(elements[i]) === -1) {
-      elements[i].enable();
+    if (disabledElements.indexOf(this.childNodes[i]) === -1) {
+      this.childNodes[i].enable();
     }
   }
 
