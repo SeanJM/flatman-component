154a946382ce34d0451ff3a29f4e4f628e1d1845
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-06 09:01:53 -0500

    'init' adds on and once assignment

--- src/main/Component/Component.prototype.init.js
+++ src/main/Component/Component.prototype.init.js
@@ -1,8 +1,19 @@
 Component.prototype.init = function (opt) {
   this.node = {};
   this.childNodes = [];
-  this.dict = Object.assign({
+
+  this.dict = {
     disabledElements : [],
     isDisabled : false
-  }, opt);
+  };
+
+  for (var k in opt) {
+    if (k.substr(0, 4) === 'once') {
+      this.once(k.substr(4), opt[k]);
+    } else if (k.substr(0, 2) === 'on') {
+      this.on(k.substr(2), opt[k]);
+    } else {
+      this.dict[k] = opt[k];
+    }
+  }
 };
