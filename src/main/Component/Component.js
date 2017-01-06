70a15806b7ee77ccef375167a6851f0f7020b712
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-06 09:00:58 -0500

    added 'flattman' detection for building prototype methods

--- src/main/Component/Component.js
+++ src/main/Component/Component.js
@@ -5,10 +5,23 @@ function Component() {
 }
 
 (function () {
-  var prototype = el('div').constructor.prototype;
-  for (var method in prototype) {
+  var prototype;
+  var methods = [];
+  var flatman = require && require('flatman');
+
+  if (flatman) {
+    prototype = flatman.el.constructor.prototype;
+    methods = Object.getOwnPropertyNames();
+  } else {
+    prototype = el('div').constructor.prototype;
+    for (var k in prototype) {
+      methods.push(k);
+    }
+  }
+
+  methods.forEach(function (method) {
     if (!Component.prototype[method]) {
       Component.prototype[method] = facade.component(method);
     }
-  }
+  });
 }());
