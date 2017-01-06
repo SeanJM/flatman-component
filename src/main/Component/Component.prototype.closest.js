f09231e19625310c10768b0faeff872d0592615a
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-06 09:01:39 -0500

    closest works with 'parentComponent'

--- src/main/Component/Component.prototype.closest.js
+++ src/main/Component/Component.prototype.closest.js
@@ -1,11 +1,11 @@
 Component.prototype.closest = function (constructor) {
-  var p = this.parentNode;
+  var p = this.parentComponent;
 
   while (p) {
     if (p instanceof constructor) {
       return p;
     }
-    p = p.parentNode;
+    p = p.parentComponent;
   }
 
   return false;
