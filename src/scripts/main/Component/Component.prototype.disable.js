45c152d72b26cc98ae0a0d4d3bbaa19b7bf941dc
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-11 20:31:40 -0500

    simplified 'enable' and 'disable'

--- src/scripts/main/Component/Component.prototype.disable.js
+++ src/scripts/main/Component/Component.prototype.disable.js
@@ -1,14 +1,7 @@
 Component.prototype.disable = function () {
-  var i = 0;
-  var n = this.childNodes.length;
-
-  this.dict.disabledElements = this.childNodes.filter(function (a) { return a.dict.isDisabled; });
-  this.dict.isDisabled = true;
-
-  for (; i < n; i++) {
-    this.childNodes[i].disable();
-  }
-
   this.node.document.disable();
+  this.node.document.childNodes.forEach(function (a) {
+    a.disable();
+  });
   return this;
 };
--- src/scripts/main/Component/Component.prototype.enable.js
+++ src/scripts/main/Component/Component.prototype.enable.js
@@ -1,16 +1,7 @@
 Component.prototype.enable = function () {
-  var i = 0;
-  var n = this.childNodes.length;
-  var disabledElements = this.dict.disabledElements;
-
-  this.dict.isDisabled = false;
-
-  for (; i < n; i++) {
-    if (disabledElements.indexOf(this.childNodes[i]) === -1) {
-      this.childNodes[i].enable();
-    }
-  }
-
   this.node.document.enable();
+  this.node.document.childNodes.forEach(function (a) {
+    a.enable();
+  });
   return this;
 };
