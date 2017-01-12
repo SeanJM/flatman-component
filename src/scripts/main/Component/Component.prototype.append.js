1fca517b2a8e8331cb71eb6cf1d5944bf0db3095
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-12 08:44:40 -0500

    append sends a node to be appended instead of the component itself

--- src/scripts/main/Component/Component.prototype.append.js
+++ src/scripts/main/Component/Component.prototype.append.js
@@ -1,10 +1,16 @@
 Component.prototype.append = function (children) {
   var self = this;
+  var root = this.node.document;
 
   this.childNodes = this.childNodes || [];
 
   this.mapChildrenToNode(children);
-  this.node.document.append(children);
+
+  root.append(children.map(function (a) {
+    var node = getNode(a);
+    a.parentNode = root;
+    return node;
+  }));
 
   children.forEach(function (child) {
     child.parentComponent = self;
