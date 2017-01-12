d55e3202e8b6e75b0ccdd72bacef67872453dbb8
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-12 13:46:46 -0500

    pretty printing

--- src/scripts/main/Component/Component.prototype.appendTo.js
+++ src/scripts/main/Component/Component.prototype.appendTo.js
@@ -1,6 +1,10 @@
 Component.prototype.appendTo = function (target) {
-  target = typeof el === 'function' ? el(target) : target;
+  target = typeof el === 'function'
+    ? el(target)
+    : target;
+
   this.node.document.appendTo(target);
   this.parentNode = target;
+
   return this;
 };
