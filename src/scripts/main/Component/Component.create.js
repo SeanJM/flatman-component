8608254bfe7db8372e2845edd278ea6d4d6162ed
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-21 19:11:29 -0500

    Added names guard

--- src/scripts/main/Component/Component.create.js
+++ src/scripts/main/Component/Component.create.js
@@ -1,53 +1,69 @@
-Component.create = function (name, methods) {
-  var methodList = [];
-  var C = function () {};
-
-  function Constructor(fn) {
-    return function (opt) {
-      fn.call(this, opt);
-    };
+Component.create = function (name) {
+  function createFunction(thunk) {
+    Component.function[name] = thunk;
   }
 
-  function wrapper(k) {
-    return function () {
-      var i = 0;
-      var n = arguments.length;
-      var $arguments = new Array(n);
-      var result;
+  function createConstructor(methods) {
+    var methodList = [];
+    var C = function () {};
 
-      for (;i < n; i++) {
-        $arguments[i] = arguments[i];
+    function Constructor(fn) {
+      return function (opt) {
+        fn.call(this, opt);
+      };
+    }
+
+    function wrapper(k) {
+      return function () {
+        var i = 0;
+        var n = arguments.length;
+        var $arguments = new Array(n);
+        var result;
+
+        for (;i < n; i++) {
+          $arguments[i] = arguments[i];
+        }
+
+        result = methods[k].apply(this, $arguments);
+
+        return result;
+      };
+    }
+
+    for (var k in methods) {
+      if (methods.hasOwnProperty(k)) {
+        methodList.push(k);
+        if (k === 'constructor') {
+          C = Constructor(methods[k]);
+        }
       }
+    }
 
-      result = methods[k].apply(this, $arguments);
-
-      return result;
-    };
-  }
-
-  for (var k in methods) {
-    if (methods.hasOwnProperty(k)) {
-      methodList.push(k);
-      if (k === 'constructor') {
-        C = Constructor(methods[k]);
+    for (method in methods) {
+      if (method === 'append') {
+        C.prototype.append = Component.facade.append(methods[method]);
+      } else if (method !== 'constructor') {
+        C.prototype[method] = wrapper(method);
       }
     }
-  }
 
-  for (method in methods) {
-    if (method === 'append') {
-      C.prototype.append = Component.facade.append(methods[method]);
-    } else if (method !== 'constructor') {
-      C.prototype[method] = wrapper(method);
+    for (var method in Component.prototype) {
+      if (typeof C.prototype[method] === 'undefined') {
+        C.prototype[method] = Component.prototype[method];
+      }
     }
+
+    Component.lib[name] = C;
+    return C;
   }
 
-  for (var method in Component.prototype) {
-    if (typeof C.prototype[method] === 'undefined') {
-      C.prototype[method] = Component.prototype[method];
-    }
+  if (Component.lib[name] || Component.function[name]) {
+    throw 'Cannot create Component function: duplicate name \'' + name + '\'';
   }
 
-  Component.lib[name] = C;
-  return C;
+  if (typeof arguments[1] === 'function') {
+    return createFunction(arguments[1]);
+  } else {
+    return createConstructor(arguments[1]);
+  }
 };
