3f0f36c3ef6dbd93b22f6a7aa03e969c248733e8
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-03-16 11:20:59 -0400

    Optimized 'mapChildrenToNode'

--- src/scripts/main/Component/Component.prototype.mapChildrenToNode.js
+++ src/scripts/main/Component/Component.prototype.mapChildrenToNode.js
@@ -1,12 +1,18 @@
 Component.prototype.mapChildrenToNode = function (children) {
   var self = this;
-  children = Array.isArray(children) ? children : [children];
-  children.forEach(function (child) {
-    var name = child.name && child.name();
-    if (name) {
-      self.node[name] = child;
+  var name;
+
+  children = Array.isArray(children)
+    ? children
+    : [ children ];
+
+  for (var i = 0, n = children.length; i < n; i++) {
+    name = children[i].name && children[i].name();
+
+    if (name && !self.node[name]) {
+      self.node[name] = children[i];
     }
-  });
+  }
 
   return this;
 };
