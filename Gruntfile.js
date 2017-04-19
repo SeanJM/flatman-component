ea6bb4aa22e9dada7dee22d5e7a1633ee5a9cda8
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-04-19 16:57:18 -0400

    Simplified the build script

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,14 +11,9 @@ module.exports = function(grunt) {
   grunt.initConfig(initConfig);
 
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-svgstore');
-  grunt.loadNpmTasks('grunt-autoprefixer');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   grunt.registerTask('default', tasks);
 
--- grunt/initConfig.js
+++ grunt/initConfig.js
@@ -2,36 +2,13 @@ const fs = require('fs');
 const flatman = require('./flatman');
 const readme = require('./readme');
 const scripts = require('./scripts');
-const css = require('./css');
-const images = require('./images');
-const fonts = require('./fonts');
 
 const config = JSON.parse(fs.readFileSync('grunt.json'));
 
 module.exports = {
-  copy : {
-    fonts : {
-      expand : true,
-      flatten : true,
-      src : fonts.files,
-      dest : config.dest
-    },
-    images : images.task.copy
-  },
-
-  sass : css.task.sass,
-
-  cssmin : css.task.cssmin,
-
   concat : scripts.task.concat,
 
   uglify : scripts.task.uglify,
-
-  autoprefixer : css.task.autoprefixer,
-
-  imagemin : images.task.imagemin,
-
-  svgstore : images.task.svgstore,
 
   watch : config.isProduction
     ? {}
