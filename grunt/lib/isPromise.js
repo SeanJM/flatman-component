09364d7dd8d56e53866deff066e5dcba7c69d329
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-07-14 20:02:51 -0400

    removed grunt build

--- grunt/lib/isPromise.js
+++
@@ -1,9 +0,0 @@
-function isPromise(obj) {
-  return (
-    typeof obj !== 'undefined'
-    && typeof obj.then === 'function'
-    && typeof obj.catch === 'function'
-  );
-}
-
-module.exports = isPromise;
