31d7272d90d070df2371723d567d102504f0fdc5
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-08-15 14:39:32 -0400

    Changed reference to "node" to "names"

--- index.js
+++ index.js
@@ -333,12 +333,12 @@ Component.prototype.mapChildrenToNode = function (children) {
   for (var i = 0, n = children.length; i < n; i++) {
     name = children[i].name && children[i].name();
     children[i].parentNode = this;
-    if (name && !this.node[name]) {
-      this.node[name] = children[i];
+    this.childNodes.push(children[i]);
+    if (name && !this.names[name]) {
+      this.names[name] = children[i];
     }
   }
 
-  [].push.apply(this.childNodes, children);
   return this;
 };
 
